fbb0a61e00cb9aed013f32b2d445f003beab84f6
Arthur Krom <arthurkrom@hotmail.com>
2023-12-02 16:03:29 +0100

    Completed part 2 of cube-conundrum

--- day-2-cube-conundrum/part-2/cube-conundrum.js
+++ day-2-cube-conundrum/part-2/cube-conundrum.js
@@ -9,51 +9,63 @@ for await (const line of file.readLines()) {
     let str = `Game ${++gameCount}:`;
     let data = line.slice(line.indexOf(str) + str.length);
     let sets = data.split(";");
-    let isImpossible = false;
+
+    // highest nrs per game
+    let highestRed = 0;
+    let highestGreen = 0;
+    let highestBlue = 0;
 
 
-    for (const element of sets) {
-        if (isImpossible) { // quit when an impossible set has already been found
-            break;
+    sets.forEach(set => {
+        let [r, g, b] = calculateLowestPerSet(set);
+
+        if(r > highestRed){
+            highestRed = r;
         }
-        isImpossible = setContainsImpossible(element);
-    }
 
-    if (isImpossible) {
-        console.log("Impossible: ", line);
-    } else {
-        console.log("Possible: ", line);
-        result += gameCount;
-    }
+        if(g > highestGreen){
+            highestGreen = g;
+        }
+
+        if(b > highestBlue){
+            highestBlue = b;
+        }
+    });
+    result += highestRed * highestGreen * highestBlue;
 }
 
-console.log(result)
+console.log(result);
 
 /**
- *
- * set can be "
- * @param set {string}
- * @return {boolean}
+ * set can be 5 red, 3 green, 4 blue]
+ * @param set
+ * @return {number[]}
  */
-function setContainsImpossible(set) {
-    console.log(set)
+function calculateLowestPerSet(set) {
+    let highestRed = 0;
+    let highestGreen = 0;
+    let highestBlue = 0;
+
     const COLOR_AMOUNT_INDEX = 0;
     const COLOR_NAME_INDEX = 1;
-    const LIMITS = [{color: "red", limit: 12}, {color: "green", limit: 13}, {color: "blue", limit: 14}];
-
-    let result = false;
 
     let balls = set.split(",");
 
     balls.forEach(ball => {
         let valAndColor = ball.trim().split(" ");
-        let found = LIMITS.find((l) => {
-            return l.color === valAndColor[COLOR_NAME_INDEX];
-        });
-        if (valAndColor[COLOR_AMOUNT_INDEX] > found.limit) {
-            result = true;
+
+        if(valAndColor[COLOR_NAME_INDEX] === "red"){
+            highestRed = parseInt(valAndColor[COLOR_AMOUNT_INDEX]);
+        }
+
+        if(valAndColor[COLOR_NAME_INDEX] === "green"){
+            highestGreen = parseInt(valAndColor[COLOR_AMOUNT_INDEX]);
+        }
+
+        if(valAndColor[COLOR_NAME_INDEX] === "blue"){
+            highestBlue = parseInt(valAndColor[COLOR_AMOUNT_INDEX]);
         }
     });
 
-    return result;
-};
+    return [highestRed, highestGreen, highestBlue];
+}
